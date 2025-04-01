5398ccd86c7f87bbb10af4c714786f823a628d3c
b100100b <jihyenog.baek@gmail.com>
2025-04-01 17:35:05 +0900

    04-01 Homework

--- hw4.py
+++ hw4.py
@@ -2,23 +2,17 @@ def rep_char(c, n):
     print(c * n)
 
 def draw_line_string(stri):
-    msg1 = f'Welcome {stri}'
-    msg2 = 'Welcome to Seoul.'
+    msg1 = f' Welcome {stri}, '
+    msg2 = ' Welcome to Seoul. '
 
-    nstr = len(msg1)
+    nstr = len(msg1) if (len(msg1) > len(msg2)) else len(msg2)
 
-    if nstr > len(msg2):
-        rep_char('-', nstr)
-        print(msg1)
-        print(msg2)
-        rep_char('-', nstr)
-
-    else:
-        rep_char('-', len(msg2))
-        print(msg1)
-        print(msg2)
-        rep_char('-', len(msg2))
+    rep_char('-', nstr)
+    print(msg1)
+    print(msg2)
+    rep_char('-', nstr)
 
 name = input('input name:')
 
 draw_line_string(name)
+
